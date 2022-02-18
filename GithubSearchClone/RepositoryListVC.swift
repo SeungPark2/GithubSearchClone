8a27bed7eadb4c63492d4dc7ede2d98e4e3dec01
SeungPark2 <skunow2@naver.com>
2022-02-18 17:11:30 +0900

    init searchController

--- GithubSearchClone/RepositoryListVC.swift
+++ GithubSearchClone/RepositoryListVC.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class RepositoryListVC: UIViewController {
 
+    // MARK: -- Public Properties
+    
+    // MARK: -- Public Method
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        initSearchController()
     }
+    
+    // MARK: -- Private Method
+    
+    private func initSearchController() {
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "저장소명 또는 소유자명 검색"
+        searchController.searchBar.setValue("취소",
+                                            forKey: "cancelButtonText")
+        searchController.searchBar.tintColor = .white
+        self.navigationItem.searchController = searchController
+        
+        self.navigationItem.hidesSearchBarWhenScrolling = true
+    }
+    
+    // MARK: -- Private Properties
+    
+    // MARK: -- IBOutlet
+    
+    @IBOutlet private weak var loginButton: UIBarButtonItem?
+    
+    @IBOutlet private weak var repositoryTableView: UITableView?
 }
 
---
+++ GithubSearchClone/RepositoryReactor.swift
@@ -0,0 +1,8 @@
+//
+//  RepositoryReactor.swift
+//  GithubSearchClone
+//
+//  Created by 박승태 on 2022/02/18.
+//
+
+import Foundation
